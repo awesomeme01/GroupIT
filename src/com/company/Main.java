2ce7ec4d29e5154653948f10c12514416f04dce3
Astik <serikovastik@mail.com>
2019-01-12 15:14:10 +0600

    Hello commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-
+        System.out.println("Hello team!");
         System.out.println("Welcome to the group work!!");
     }
 }
